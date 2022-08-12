4238a1fe6a8c9dba847911e420dc3060edc57756
sdivakaran143 <sdivakaran683@gmail.com>
2022-08-12 10:15:06 +0530

    modified

--- remove_the_before_and_after_char_of_star.java
+++ remove_the_before_and_after_char_of_star.java
@@ -8,20 +8,31 @@ abcfge
  */
 import java.util.*;
 
-public class prgm90 {
+public class remove_the_before_and_after_char_of_star {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Enter the string : ");
         String str = in.nextLine();
-        str = str.replace('*', ' ');
-        String[] st = str.split(" ");
-        st[0] = " " + st[0];
-        str = "";
-        for (int i = 0; i < st.length; i++) {
-            if (st[i].length() > 2) {
-                str += st[i].substring(1, st[i].length() - 1);
+        for (int i = 0; i < str.length(); i++) {
+            char ch=str.charAt(i);
+            if(ch=='*'){
+            str=str.substring(0, i)+str.substring(i+1);
+            if(i==0){
+                str=str.substring(i+1);
+                i=0;
+                continue;
             }
+            if(i==str.length()-1){
+                str=str.substring(0, i);
+                i=0;
+                continue;
+            }
+            else{
+                str=str.substring(0, i-1)+str.substring(i+1);
+                i=0;
+            }
+            } 
         }
         System.out.println("\nOutput : " + str);
     }
