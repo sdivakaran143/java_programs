b9684a33b7708533dad54bbafad26d546f2cae31
sdivakaran143 <sdivakaran683@gmail.com>
2022-08-12 12:23:47 +0530

    new

--- remove_the_before_and_after_char_of_star.java
+++ remove_the_before_and_after_char_of_star.java
@@ -17,14 +17,14 @@ public class remove_the_before_and_after_char_of_star {
         for (int i = 1; i < str.length()-1; i++) {
             char ch=str.charAt(i);
             if(ch=='*'){
-                str=str.substring(0,i-1)+" "+str.substring(i+1);
+                str=str.substring(0,i-1)+"  "+str.substring(i+1);
                 if(str.charAt(i+1)!='*'){
-                    str=str.substring(0,i-1)+" "+str.substring(i);
+                    str=str.substring(0,i+1)+" "+str.substring(i+2);
                 }
             }
             
         } 
-
+        str=str.replace(" ","")
         System.out.println("\nOutput : " + str);
     }
 }
