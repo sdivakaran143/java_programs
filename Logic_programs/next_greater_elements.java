d312aae8a8fb5e8842961704fa8f0fa60eaacdd6
sdivakaran143 <sdivakaran683@gmail.com>
2022-08-19 10:02:29 +0530

    19.8.22

--- Logic_programs/next_greater_elements.java
+++ Logic_programs/next_greater_elements.java
@@ -2,18 +2,20 @@ import java.util.*;
 public class next_greater_elements {
     static Scanner in =new Scanner(System.in);
     public static void main(String[] args) {
-        int[] arr ={4, 5, 2, 25};
+        int[] arr ={9,9,4,2,8 ,0};
         boolean key =true;
         for(int i=0;i<arr.length;i++){
             key=true;
-            for(int j=i+1;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
                 if(arr[i]<arr[j]){
                     System.out.println(arr[i]+" = "+arr[j]);
                     key=false;
                     break;
                 }
-            } if(key && j==arr.length-1){
+            } 
+            if(key && i==arr.length-1||key){
                 System.out.println(arr[i]+" = -1");
+                key=false;
             }
         }
     }
