9370739e9abd6d54c3d23d5f9b464db059ecb480
Divakaran_Senthilkumar <109328372+sdivakaran143@users.noreply.github.com>
2022-08-24 15:36:27 +0530

    Add files via upload

---
+++ ip_validation.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.util.regex.*;
+
+class ip_validation {
+    static Scanner in = new Scanner(System.in);
+    static boolean res = true;
+
+    public static void call(Boolean val) {
+        if (val) {
+            System.out.println("false...");
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter the IP to validate : ");
+        String ip = in.nextLine();
+        res = Pattern.compile("[a-z]").matcher(ip).find();
+        call(res);
+        String[] str = ip.split("[.]");
+        if (str.length != 4) {
+            call(true);
+        }
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].length() > 4 || Integer.parseInt(str[i]) > 255) {
+                call(true);
+            }
+        }
+        System.out.println("TRUE...");
+    }
+}
