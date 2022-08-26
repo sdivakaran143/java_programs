a8a86061b26b782d823a0a3900b72b7432cc87e7
sdivakaran143 <sdivakaran683@gmail.com>
2022-08-26 11:08:03 +0530

    25.8.2022

--- Power_of_number_without_using_star_and_slash_Operator.java
+++ Power_of_number_without_using_star_and_slash_Operator.java
@@ -1,18 +1,21 @@
 import java.util.*;
 public class Power_of_number_without_using_star_and_slash_Operator {
  static Scanner in =new Scanner(System.in);
+        public static int mul(int val,int times) {
+            int n=val;
+               for (int j = 1; j <times; j++) {
+                   val+=n;
+               }
+               return val;
+        }
     public static void main(String[] args) {
         System.out.print("enter the base :");
         int b=in.nextInt();
         System.out.print("enter the power :");
         int p=in.nextInt();
-        int val=0,m=b;
-        for (int i = 0; i < p; i++) {
-            for (int j = 0; j <b; j++) {
-                m+=m;
-            }
-            val+=m;
-            m=val;
+        int val=b;
+        for (int i = 1; i <p; i++) {
+            val=mul(val, b);
         }
         System.out.println(val);
     }   
