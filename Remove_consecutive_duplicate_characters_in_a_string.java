c5d89c4d1e85cafea2ef52ab7fea16d0f4280620
sdivakaran143 <sdivakaran683@gmail.com>
2022-08-26 17:45:31 +0530

    26.8.2022

--- Remove_consecutive_duplicate_characters_in_a_string.java
+++ Remove_consecutive_duplicate_characters_in_a_string.java
@@ -2,6 +2,13 @@ import java.util.*;
 public class Remove_consecutive_duplicate_characters_in_a_string {
     static Scanner in =new Scanner(System.in);
     public static void main(String[] args) {
-        String str =in.nextLine();
+        String str =" "+in.nextLine()+" ";
+        System.out.print("OUTPUT : ");
+        for (int i = 1; i < str.length()-1; i++) {
+            char ch=str.charAt(i);
+            if(str.charAt(i-1)!=ch&&ch!=str.charAt(i+1)){
+                System.out.print(ch);
+            }
+        }
     }
 }
