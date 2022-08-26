45d12d9ad8536355c54144989dd0d691323c01a6
sdivakaran143 <sdivakaran683@gmail.com>
2022-08-26 17:54:20 +0530

    26.8.2022

---
+++ array_union_common_sort_intersection.java
@@ -0,0 +1,15 @@
+public class array_union_common_sort_intersection {
+    public static void main(String[] args) {
+        int arr1[]={ 12 , 45 , 62,  98 , 5 , 28};
+        int arr2[]={ 98 , 23 , 12 ,145};
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if(arr1[i]==arr2[j]){
+
+                }
+            }
+        }
+
+    }
+}
