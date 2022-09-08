78393cd6cd787a692071c7777250975670f6450b
sdivakaran143 <sdivakaran683@gmail.com>
2022-09-08 12:13:14 +0530

    8.9.2022

--- to_print_fibnocci_in_prymid.java
+++ to_print_fibnocci_in_prymid.java
@@ -7,11 +7,11 @@ public class to_print_fibnocci_in_prymid {
         int a=-1,b=1,n=0;
         while(true) {
             for (int i = 0; i < row-n; i++) {
-                System.out.print("  ");
+                System.out.print("   ");
             }
             for (int j = 0; j < n; j++) {
                 int c=a+b;
-                 System.out.printf("%-5s",c);
+                 System.out.printf("%-7s",c);
                  a=b;b=c;
             }
             System.out.println();
