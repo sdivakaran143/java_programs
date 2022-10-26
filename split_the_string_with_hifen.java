080a655058fd85c68e2805027075a1a11798890d
sdivakaran143 <sdivakaran683@gmail.com>
2022-10-26 11:23:40 +0530

    26.10.2022

--- split_the_string_with_hifen.java
+++ split_the_string_with_hifen.java
@@ -2,10 +2,15 @@ import java.util.*;
 public class split_the_string_with_hifen{
     static Scanner in=new Scanner(System.in);
     public static void main(String[] args) {
-        String str="2-5g-3-j-";int m=0;
+        System.out.print("enter the String : ");
+        String str=in.nextLine();
+        System.out.print("Enter the split values : ");
+        int spvalue=in.nextInt();
+        //String str="2-5g-3-j-jjj";
+        int m=0;
         str=str.replace("-", "");
         for (int i = 0; i < str.length(); i++) {
-            if(m==2){
+            if(m==spvalue){
                 System.out.print("-");
                 m=0;
             }
