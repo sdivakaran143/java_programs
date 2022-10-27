fbeec54295285d87616f0cb433da4d7fa245ed47
sdivakaran143 <sdivakaran683@gmail.com>
2022-10-27 10:24:47 +0530

    27.10.22

--- Logic_programs/match_substring_with_given_stuing.java
+++ Logic_programs/match_substring_with_given_stuing.java
@@ -1,12 +1,12 @@
 public class match_substring_with_given_stuing {
     public static void main(String[] args) {
         String match="fi_er";
-        String str="flier:filter:fixer:tailor:offers";
+        String str="filer:filter:fixer:tailor:offers";
         String marr[]=match.split("_");
         String sarr[]=str.split(":");
         for (String s : sarr) {
-            if(s.substring(s.length()-2).equals(marr[1]) && s.substring(0,2).equals(marr[0]) && s.length()==5){
-                System.out.println(s);
+            if(s.substring(0,2).equals(marr[0]) && s.substring(s.length()-2).equals(marr[1])&& s.length()==5){
+                System.out.print(s+":");
             }
         }
     }
