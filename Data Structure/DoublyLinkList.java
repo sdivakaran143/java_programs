bf8503d760634a25575cb3ee74c13871518abd12
sdivakaran143 <sdivakaran683@gmail.com>
2022-10-29 15:21:36 +0530

    29.10.2022

--- Data Structure/DoublyLinkList.java
+++ Data Structure/DoublyLinkList.java
@@ -1,4 +1,6 @@
+import java.util.*;
 public class DoublyLinkList {
+    static Scanner in =new Scanner(System.in);
     public Node head;
     class Node{
         int data ;
@@ -6,7 +8,39 @@ public class DoublyLinkList {
         Node right;
         Node(int data){
             this.data=data;
-            this.left=
+            this.left=null;
+            this.right=null;
         }
+    }
+    public void addfirst(int data){
+        Node newnode=new Node(data);
+        if(head==null){
+            head=newnode;
+        }
+        head.right=newnode;
+        head=newnode;
+    }
+    
+    public void addLast(int data){
+        Node newnode=new Node(data);
+        if(head==null){
+            head=newnode;
+        }
+        head.right=newnode;
+        head=newnode;
+    }
+    
+    public void print(){
+        Node curnode=head;
+        System.out.print("Null");
+        while(curnode.right!=null){
+            System.out.print(curnode.data+" -> ");
+            curnode=curnode.right;
+        } 
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkList dl=new DoublyLinkList();
+        dl.addfirst(10);
     }
 }
