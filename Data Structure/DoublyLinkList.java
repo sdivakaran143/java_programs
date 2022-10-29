d66ebfb1ab6cb8036f121bb4042d025ed5971568
sdivakaran143 <sdivakaran683@gmail.com>
2022-10-29 16:34:26 +0530

    29.10.2022

--- Data Structure/DoublyLinkList.java
+++ Data Structure/DoublyLinkList.java
@@ -15,32 +15,65 @@ public class DoublyLinkList {
     public void addfirst(int data){
         Node newnode=new Node(data);
         if(head==null){
-            head=newnode;
+            head=newnode;return;
         }
-        head.right=newnode;
+        newnode.right=head;
+        head.left=newnode;
         head=newnode;
     }
     
-    public void addLast(int data){
-        Node newnode=new Node(data);
+   
+    public void addlast(int data){
+        Node newnode =new Node(data);
         if(head==null){
-            head=newnode;
+            head=newnode;return;
         }
-        head.right=newnode;
-        head=newnode;
+        Node curnode=head;
+        while(curnode.right!=null){
+          curnode=curnode.right;
+        }
+        curnode.right=newnode;
+        newnode.left=curnode;
     }
     
     public void print(){
+        if(head==null){
+            System.out.print("List is Empty....");return;
+        }
         Node curnode=head;
-        System.out.print("Null");
-        while(curnode.right!=null){
+        System.out.print("Null -> ");
+        while(curnode!=null){
             System.out.print(curnode.data+" -> ");
             curnode=curnode.right;
         } 
+        System.out.print(" Null");
+    }
+    public void deletelast(){
+        if(head==null){
+            System.out.print("List is Empty....");return;
+        }
+        Node curNode=head;
+        while(curNode.right.right!=null){
+            curNode=curNode.right;
+        }
+        curNode.right=null;
+    }
+    public void deletefirst(){
+        Node curNode=head;
+        head=curNode.right;
     }
 
     public static void main(String[] args) {
-        DoublyLinkList dl=new DoublyLinkList();
-        dl.addfirst(10);
+        DoublyLinkList dll=new DoublyLinkList();
+        dll.addfirst(10);
+        dll.addlast(20);
+        dll.addlast(30);
+        dll.addfirst(55);
+        dll.addlast(35);
+        dll.deletefirst();
+        dll.deletefirst();
+
+
+        dll.print();
     }
 }
