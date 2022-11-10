ff00be58f362970771c02e16f9484a9ab0ba936a
sdivakaran143 <sdivakaran683@gmail.com>
2022-11-10 15:08:49 +0530

    10.11.2022

---
+++ Queries_on_subsequence_of_string.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Queries_on_subsequence_of_string {
+    static Scanner in=new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("enter the String to check : ");
+        String str = in.nextLine();
+        String arr[] ={"gg","gro","gfg","orf"};
+        for (int i = 0; i < arr.length; i++) {
+            int k=0;
+            String str1 =str;
+            for (int j = 0; j < str.length(); j++) {
+                int m=str1.indexOf(arr[i].charAt(k));
+                if(m!=-1){
+                    str1=str1.substring(m+1); 
+                }else{
+                    System.out.println("No");
+                    break;
+                }
+                if(k==arr[i].length()-1){
+                    System.out.println("Yes");
+                    break;
+                }k++;
+            }
+        }    
+    }
+}
