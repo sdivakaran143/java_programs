961a3251aa438bdeec4b76f46588ad75810f5166
sdivakaran143 <sdivakaran683@gmail.com>
2023-02-09 08:45:37 +0530

    09-02-23

--- BasicPrograms/Build_Lowest_Number_by_Removing_n_digits.java
+++ BasicPrograms/Build_Lowest_Number_by_Removing_n_digits.java
@@ -3,33 +3,27 @@ public class Build_Lowest_Number_by_Removing_n_digits {
     static Scanner in =new Scanner(System.in);
     public static void main(String[] args) {
         String str = "765028321";
-                    //012345678
+                    //60221
+                    
         // String str = "121198";
-        int n = 5;
-        char[] ch= str.toCharArray();
-        Arrays.sort(ch);
-        int len=str.length()-n;
-        int arr[]=new int[ch.length];
-        String str1=str;
-        for (int i = 0,j=0; i < ch.length; i++) {
-            int val=str.indexOf(ch[i],arr[j]);
-            if(val!=-1){
-                arr[j]=val;
-                str=str.substring(0,arr[j])+"U"+str.substring(arr[j]+1);
-                j++;
-            }
+                       //1118
+        
+        String output="";
+for (int i = 0; i < str.length()-1; i+=2) {
+    int n=Integer.parseInt(str.charAt(i)+"");
+    int m=Integer.parseInt(str.charAt(i+1)+"");
+    System.out.println(n+""+m);
+    if(n<m){
+            output+=str.charAt(i);
+    }else{
+            output+=str.charAt(i+1);
         }
-        String Output="";
-        // Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            Output+=str1.charAt(arr[i]);
-
-            // if(arr[i-1]<arr[i]){
-            //     // Output+=str1.charAt(arr[i]);
-            // }
-        }
-        System.out.println();
-        System.out.println(Output);
+        if(i+2>str.length()-1)
+            if(str.length()%2==1)
+                output+=str.charAt((str.length()-1));
+        // System.out.println("hh");
+}
+        
+        System.out.println(output);
     }
 }
